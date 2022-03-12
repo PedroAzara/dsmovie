20967e21e6a8fd30bb93b858e1c597dd5d317039
Pedro Henrique <pedrohenriquealmeida2004@gmail.com>
2022-03-12 15:16:36 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,45 +1,42 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { useEffect, useState } from "react";
-import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
-    const [page, setPage] = useState<MoviePage>(
-        {
-            content: [],
-            last: true,
-            totalPages: 0,
-            totalElements: 0,
-            size: 12,
-            number: 0,
-            first: true,
-            numberOfElements: 0,
-            empty: true,
-        }
-    );
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
     useEffect(() => {
-
-        //Se der qualquer erro, arrumar esse "score"
-        axios.get(`${BASE_URL}/movies?size=12&page=page=${pageNumber}&sort=id`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
-                const data = response.data as MoviePage
+                const data = response.data as MoviePage;
                 setPage(data);
+            });
+    }, [pageNumber]);
 
-            })
-    }, [pageNumber])
-
-
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
+            <Pagination page={page} onChange={handlePageChange} />
 
-            <Pagination />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
@@ -48,13 +45,10 @@ function Listing() {
                         </div>
                     )
                     )}
-
-
                 </div>
             </div>
-
         </>
-    )
+    );
 }
 
 export default Listing;
